15bec39e15ad3ed715e6b29c124580e6577cfb36
JesusKid <frawllings@gmail.com>
2021-12-05 18:51:29 +0100

    added requirement

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import base64
 import os
 
 app = Flask(__name__)
-app.secret_key = 'ksjkdbjhwvryf7ksmdms'
+app.secret_key = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', "sqlite:///data.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
